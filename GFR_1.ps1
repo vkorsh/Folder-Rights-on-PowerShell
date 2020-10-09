d23792ae44883b1dd7243caeb35479def93aac77
vkorsh <vkorsh@users.noreply.github.com>
2020-10-09 15:07:03 +0300

    Update GFR_1.ps1

--- GFR_1.ps1
+++ GFR_1.ps1
@@ -1,12 +1,12 @@
-#Дизайн консоли - меняем цвет консоли на черный 
+# Дизайн консоли - меняем цвет консоли на черный 
 [console]::BackgroundColor = "Black"
-#Очищаем консоль перед использованием
+# Очищаем консоль перед использованием
 Clear-Host
-
+# Легкая раскараска консоли и вывод заголовка
 Write-Host `n "Folder Rights" `n -Backgroundcolor DarkGreen
-#Получаем данные из буфера обмена
+# Получаем данные из буфера обмена
 $Folder = Get-Clipboard
-#Обрабатываем данные скриптом
+# Обрабатываем данные скриптом
 (get-acl $Folder).Access | Format-Table -Property FileSystemRights, AccessControlType, IdentityReference
-
+# Выводим данные и ждем нажатия любой клавиши для закрытия окна
 Read-Host "Press any Key"
